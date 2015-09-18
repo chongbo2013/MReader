b18c45464408c5a33ec134929a41a63a4aa4c3e2
lijing.lj <lijing.lj@alibaba-inc.com>
2015-09-18 16:32:47 +0800

    Add Abount page

---
+++ MReader/src/com/midcore/reader/activity/AbountActivity.java
@@ -0,0 +1,15 @@
+package com.midcore.reader.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.midcore.reader.R;
+
+public class AbountActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setGLContentView(R.layout.activity_abount);
+	}
+}
